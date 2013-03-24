2f326e5616486132a2be5c08d56e3890303ab3bc
Julia Teitelbaum <julialt@gmail.com>
2013-03-24 14:20:03 -0400

    This is the initial rails application. I haven't added anything. This is only what rails generates by default.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakebook::Application.config.secret_token = '4d782d724af73b6871e195bec8352ead2fa83d60cb85de146b75e6b122b05b0feb4d31016cf2c3cdaaf93a05b979db3c1a13e463d989bd9e9e74d365cc730486'
