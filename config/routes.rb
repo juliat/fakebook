1e84ebcb64461c8efec922c9f6d3cd61a4369c4d
Kevin Schaefer <kevinjschaefer@gmail.com>
2013-03-29 17:34:54 -0400

    Removed controllers, routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Fakebook::Application.routes.draw do
   resources :posts, :users, :user_sessions, :friend_requests, :friendships
-  match 'login' => 'user_sessions#new', :as => :login
-  match 'logout' => 'user_sessions#destroy', :as => :logout
-  match 'friend_requests/:id' => 'friend_requests#accept'
 
 
   # The priority is based upon order of creation:
@@ -54,7 +51,7 @@ Fakebook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  #root :to => 'public/index.html'
 
   # See how all your routes lay out with "rake routes"
 
